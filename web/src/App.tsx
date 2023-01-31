22ea6594bdb740bb9f34a6ff86276f92659303bd
Jean do Nascimento <jeandonascmento@gmail.com>
2023-01-31 20:24:21 -0300

    web commit

---
+++ web/src/App.tsx
@@ -0,0 +1,40 @@
+
+import './styles/global.css';
+import './lib/dayjs'
+import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
+import { api } from './lib/axios';
+
+navigator.serviceWorker.register('service-worker.js')
+.then(async serviceWorker => {
+  let subscription = await serviceWorker.pushManager.getSubscription()
+
+    if(!subscription){
+      const publicKeyResponse = await api.get('/push/public_key')
+      
+    subscription = await serviceWorker.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: publicKeyResponse.data.publicKey,
+    })
+  }
+await api.post('/push/register', {
+    subscription,
+})
+
+await api.post('/push/send', {
+  subscription,
+})
+
+})
+
+
+export function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
+    </div>
+  )
+}
