610d9996edb851280d8d35f37042ec2442f655f4
Jean do Nascimento <jeandonascmento@gmail.com>
2023-02-02 19:12:39 -0300

    testing url

--- web/src/lib/axios.ts
+++ web/src/lib/axios.ts
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export const api = axios.create({
 
-    baseURL: 'http://localhost:3333'
+    baseURL: 'https://habits-application-7ypq8jicj-jean-do-nascimento.vercel.app'
 })
