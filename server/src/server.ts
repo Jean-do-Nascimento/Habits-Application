dda0e0216cebf41f8c41436671729a1c18b0fec8
Jean do Nascimento <jeandonascmento@gmail.com>
2023-02-08 17:58:49 -0300

    testing server changes

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,16 +3,17 @@ import cors from '@fastify/cors'
 import { appRoutes } from "./routes"
 import { notificationRoutes } from './lib/notifications-routes'
 
+
 const app = Fastify()
+const port = process.env.PORT 
 
 app.register(cors)
 app.register(appRoutes)
 app.register(notificationRoutes)
 
 
-
 app.listen({
-  port: 3333,
+  port: 3333 || port,
   host: '0.0.0.0'
 }).then(() => {
   console.log('HTTP Server running!')
