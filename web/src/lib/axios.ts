6bc2e71738a5be47eb63f4c9647c76faa0f740dc
Jean do Nascimento <jeandonascmento@gmail.com>
2023-02-08 19:42:22 -0300

    testing changes

--- web/src/lib/axios.ts
+++ web/src/lib/axios.ts
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export const api = axios.create({
 
-    baseURL: 'https://habits-application.vercel.app/habits'
+    baseURL: 'https://habits-application.vercel.app'
 })
