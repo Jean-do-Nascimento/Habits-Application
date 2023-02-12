fdba637a3ddaa11c552103068d3088ea6ae4073b
Jean do Nascimento <jeandonascmento@gmail.com>
2023-02-11 21:06:06 -0300

    server commit

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,19 +3,15 @@ import cors from '@fastify/cors'
 import { appRoutes } from "./routes"
 import { notificationRoutes } from './lib/notifications-routes'
 
-
 const app = Fastify()
-  
+
 app.register(cors)
 app.register(appRoutes)
 app.register(notificationRoutes)
 
-
 app.listen({
   port: 3333,
-  host: '0.0.0.0'
+  host: '0.0.0.0',
 }).then(() => {
   console.log('HTTP Server running!')
 })
-
-
